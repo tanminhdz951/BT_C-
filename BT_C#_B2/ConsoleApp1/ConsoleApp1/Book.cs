616b8f841412d70d93e5dbb9d390b05f06e97f0c
Your Name <you@example.com>
2022-04-19 08:01:45 +0700

    up lan dau

---
+++ BT_C#_B2/ConsoleApp1/ConsoleApp1/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_HDT_B2
+{
+    class Book : Document
+    {
+        private String author;
+        private int numerPage;
+        public Book(string Id, string nxb, int number, string author, int numberPage)
+        : base(Id, nxb, number)
+        {
+            this.Author = author;
+            this.NumerPage = NumerPage;
+        }
+        public string Author { get => author; set => author = value; }
+        public int NumerPage { get => numerPage; set => numerPage = value; }
+    }
+}
