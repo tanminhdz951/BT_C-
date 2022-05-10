ff01547594f0534613bb5d5df8ea6951466a0ae0
Your Name <you@example.com>
2022-05-10 07:33:49 +0700

    diem danh 10 thang 5

---
+++ Diem_danh_10_05/Diem_danh_10_05/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+namespace Diem_danh_10_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Mang_236 = new int[10];
+
+            Console.WriteLine("Moi nhap gia tri");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("a[{0}] = ", i);
+                Mang_236[i] = int.Parse(Console.ReadLine());
+            }
+
+
+        }
+    }
+}
