ed3d760750ccca5b1901769c804012a751501a54
Your Name <you@example.com>
2022-05-10 07:38:39 +0700

    up_diem_danh_10/05

--- Diem_danh_10_05/Diem_danh_10_05/Program.cs
+++ Diem_danh_10_05/Diem_danh_10_05/Program.cs
@@ -12,8 +12,31 @@ namespace Diem_danh_10_05
             Console.WriteLine("Moi nhap gia tri");
             for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine("a[{0}] = ", i);
+                Console.Write("a[{0}] = ", i);
                 Mang_236[i] = int.Parse(Console.ReadLine());
+            }
+            int max = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if( Mang_236[i] > max)
+                {
+                    max = Mang_236[i];
+                }  
+            }
+
+            int min = 100000000;
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (Mang_236[i] < min)
+                {
+                    min = Mang_236[i];
+                }
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("a[{0}] = {1}",i, Mang_236[i]);
             }
 
 
